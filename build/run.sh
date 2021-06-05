3465e79ea3b020de439bbb31abac5cff8494b51c
Jaewoong Kim <rorhcdream@gmail.com>
2021-06-05 17:57:02 +0900

    Update run.sh to print number of line in output file

--- build/run.sh
+++ build/run.sh
@@ -8,14 +8,16 @@ for d in ../data/* ; do
 	for q in ../query/* ; do
 		qname=$(echo $q | cut -d"/" -f 3 | cut -d"." -f 1)
 		if [[ "$qname" =~ "$dname" ]]; then
-			echo "	query $qname"
+			echo -n "	query $qname"
 			c="../candidate_set/$qname.cs"
 			trap "exit" INT
 			timeout --foreground 60 ./main/program $d $q $c > ../output/$qname.out
 			exit_status=$?
+			echo " $(wc -l ../output/$qname.out | awk '{ print $1 }')"
 			if [[ $exit_status -eq 124 ]]; then
 			    echo "time out!"
 			fi
+
 		fi
 	done
 done
